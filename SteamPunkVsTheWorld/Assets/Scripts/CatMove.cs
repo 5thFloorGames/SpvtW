4038fef1011b3812d28c5cbc3d75fa08d0cf4cb0
Vamiyn <vamiynr@gmail.com>
2015-10-15 17:03:23 +0300

    eating anim

--- SteamPunkVsTheWorld/Assets/Scripts/CatMove.cs
+++ SteamPunkVsTheWorld/Assets/Scripts/CatMove.cs
@@ -4,11 +4,14 @@ using System.Collections;
 public class CatMove : MonoBehaviour {
 	
 	private Rigidbody2D rigid;
+	private Animator animator;
+	private RuntimeAnimatorController control;
 	
 	// Use this for initialization
 	void Start () {
 		rigid = gameObject.GetComponent<Rigidbody2D>();
 		rigid.MovePosition(transform.position + (Vector3.left * (0.15f) * Time.deltaTime));
+		animator = gameObject.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
