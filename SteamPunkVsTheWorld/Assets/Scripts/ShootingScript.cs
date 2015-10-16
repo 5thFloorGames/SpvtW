4cd49f1e7ed751da4b4b7ffe6c918d59cf7c9acb
Vamiyn <vamiynr@gmail.com>
2015-10-16 23:57:35 +0300

    plant animations not syncing anymore when they stop shooting

--- SteamPunkVsTheWorld/Assets/Scripts/ShootingScript.cs
+++ SteamPunkVsTheWorld/Assets/Scripts/ShootingScript.cs
@@ -3,43 +3,53 @@ using System.Collections;
 
 public class ShootingScript : MonoBehaviour {
 
-	public GameObject bullet;
-	public int orderInLayer;
-	private bool shooting = false;
-	private Animator animator;
+    public GameObject bullet;
+    public int orderInLayer;
+    private bool shooting = false;
+    private Animator animator;
 
-	// Use this for initialization
-	void Start () {
-		orderInLayer = gameObject.GetComponentInChildren<SpriteRenderer> ().sortingOrder + 2;
-		animator = gameObject.GetComponentInChildren<Animator>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start() {
+        orderInLayer = gameObject.GetComponentInChildren<SpriteRenderer>().sortingOrder + 2;
+        animator = gameObject.GetComponentInChildren<Animator>();
+    }
 
-	void Shoot() {
-		GameObject tempBullet = (GameObject) Instantiate (bullet,transform.position + Vector3.right, Quaternion.identity);
-		tempBullet.GetComponentInChildren<SpriteRenderer> ().sortingOrder = orderInLayer;
-	}
+    // Update is called once per frame
+    void Update() {
 
-	void StartShooting() {
-		if (!shooting) {
-			InvokeRepeating("Shoot", 0.6f, 1.6f);
-			if (animator == null) {
-				animator = gameObject.GetComponentInChildren<Animator>();
-			}
-			animator.runtimeAnimatorController = Resources.Load("ShooterShooting") as RuntimeAnimatorController;
-			shooting = true;
-		}
-	}
+    }
 
-	void Stop(){
-		if (shooting) {
-			CancelInvoke ();
-			animator.runtimeAnimatorController = Resources.Load("ShooterIdling") as RuntimeAnimatorController;
-			shooting = false;
-		}
-	}
+    void Shoot() {
+        GameObject tempBullet = (GameObject)Instantiate(bullet, transform.position + Vector3.right, Quaternion.identity);
+        tempBullet.GetComponentInChildren<SpriteRenderer>().sortingOrder = orderInLayer;
+    }
+
+    void StartShooting() {
+        if (!shooting) {
+            InvokeRepeating("Shoot", 0.6f, 1.6f);
+            if (animator == null) {
+                animator = gameObject.GetComponentInChildren<Animator>();
+            }
+            shooting = true;
+            animator.runtimeAnimatorController = Resources.Load("ShooterShooting") as RuntimeAnimatorController;
+        }
+    }
+
+    void Stop() {
+        if (shooting) {
+            CancelInvoke();
+            shooting = false;
+            StartCoroutine(randomizeAnimationStart());
+        }
+
+
+    }
+
+    IEnumerator randomizeAnimationStart() {
+        yield return new WaitForSeconds(Random.Range(0.0f, 1.5f));
+        if (!shooting) {
+            animator.runtimeAnimatorController = Resources.Load("ShooterIdling") as RuntimeAnimatorController;
+        }
+    }
+
 }
