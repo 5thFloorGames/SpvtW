3efedb4baac81f569c7b1b6181cbe4f74efa4bb4
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2015-10-16 16:32:34 +0300

    new way to eat plants, way better and fixes a bunch of bugs

--- SteamPunkVsTheWorld/Assets/Scripts/CatMove.cs
+++ SteamPunkVsTheWorld/Assets/Scripts/CatMove.cs
@@ -6,6 +6,7 @@ public class CatMove : MonoBehaviour {
 	private Rigidbody2D rigid;
 	private Animator animator;
 	private RuntimeAnimatorController control;
+	private PlantDamageScript eating;
 	
 	// Use this for initialization
 	void Start () {
@@ -17,15 +18,31 @@ public class CatMove : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	}
-	
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag.Equals ("Plant") || other.gameObject.tag.Equals ("Shooter")) {
+			eating = other.gameObject.GetComponent<PlantDamageScript>();
+			Stop ();
+		}
+	}
+
 	public void Stop(){
 		rigid.velocity = Vector2.zero;
 		animator.runtimeAnimatorController = Resources.Load("CatEating") as RuntimeAnimatorController;
+		InvokeRepeating ("Eat", 1f, 0.5f);
+	}
+
+	public void Eat(){
+		if (eating != null) {
+			eating.Damaged();
+		} else {
+			CancelInvoke();
+			Go ();
+		}
 	}
 	
 	public void Go(){
 		rigid.MovePosition(transform.position + (Vector3.left * (0.15f) * Time.deltaTime));
 		animator.runtimeAnimatorController = Resources.Load("CatWalking") as RuntimeAnimatorController;
-
 	}
 }
---
+++ SteamPunkVsTheWorld/Assets/Scripts/PlantDamageScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlantDamageScript : MonoBehaviour {
+
+	public int health = 4;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void Damaged(){
+		health--;
+		if (health == 0) {
+			if(tag.Equals("Shooter")){
+				GameObject.FindGameObjectWithTag("GameController").GetComponent<GameLogic>().unregisterShooter(gameObject);
+			}
+			Destroy(gameObject);
+		}
+	}
+}
