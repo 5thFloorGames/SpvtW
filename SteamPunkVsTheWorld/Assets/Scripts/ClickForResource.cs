bafe95f7ceb570e88a92b9962c270da4b3cdff8a
Vamiyn <vamiynr@gmail.com>
2015-10-16 11:58:46 +0300

    resources grow from size 0 to their real size when they first appear)

--- SteamPunkVsTheWorld/Assets/Scripts/ClickForResource.cs
+++ SteamPunkVsTheWorld/Assets/Scripts/ClickForResource.cs
@@ -3,22 +3,28 @@ using System.Collections;
 
 public class ClickForResource : MonoBehaviour {
 
+	private bool growing;
     private bool clickable;
     private Vector3 start;
     private Vector3 finish;
     private float startTime;
     private bool selfDestructing = false;
     private float timeToDie = 4.0f;
+	private float startSize;
 
 
 	// Use this for initialization
 	void Start () {
         clickable = true;
         finish = new Vector3(-1.65f, 0.95f, 0);
+		startSize = transform.localScale.y;
+		growing = true;
+		transform.localScale = new Vector3 (0, 0, 1);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		grow();
         if (selfDestructing) {
             float size = transform.localScale.y;
             if (size < 0) {
@@ -33,7 +39,6 @@ public class ClickForResource : MonoBehaviour {
             transform.position = Vector3.Lerp(start, finish, (Time.time - startTime));
             if (Vector3.Distance(transform.position, finish) < 0.01f) {
                 selfDestructing = true;
-                //Destroy(gameObject);
             }
         }
 	}
@@ -49,5 +54,16 @@ public class ClickForResource : MonoBehaviour {
 				gameObject.GetComponent<MovementScript>().enabled = false;
 			}
         }
+	}
+
+	void grow() {
+		if (growing) {
+			float size = transform.localScale.y;
+			if (size < startSize) {
+				transform.localScale = new Vector3 ((size+Time.deltaTime), (size+Time.deltaTime), 1);
+			} else {
+				growing = false;
+			}
+		}
 	}
 }
