a4486de2463ed86ae2517b67eb4fbcd2cacaf412
Vamiyn <vamiynr@gmail.com>
2015-10-17 13:45:36 +0300

    update resource ui elements only when needed

--- SteamPunkVsTheWorld/Assets/Scripts/ResourceUIUpdater.cs
+++ SteamPunkVsTheWorld/Assets/Scripts/ResourceUIUpdater.cs
@@ -6,10 +6,12 @@ public class ResourceUIUpdater : MonoBehaviour {
 
 	private Text[] resourceTexts;
 	private GameObject resourceCloud;
+    private int previousResources;
     public int maxSize = 400;
 
 	// Use this for initialization
 	void Start () {
+        previousResources = 99;
 		resourceTexts = gameObject.GetComponentsInChildren<Text>();
         resourceCloud = transform.FindChild("ResourceCloud").gameObject;
 	}
@@ -17,15 +19,18 @@ public class ResourceUIUpdater : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         int resources = ResourceManager.getResources();
-        string resourceText = resources.ToString();
-		foreach (Text textComponent in resourceTexts) {
-            textComponent.text = resourceText;
-		}
+        if (resources != previousResources) {
+            string resourceText = resources.ToString();
+            foreach (Text textComponent in resourceTexts) {
+                textComponent.text = resourceText;
+            }
 
-        int cloudSize = resources * 10;
-        if (cloudSize > 500) {
-            cloudSize = 500;
+            int cloudSize = resources * 10;
+            if (cloudSize > 500) {
+                cloudSize = 500;
+            }
+            resourceCloud.transform.localScale = new Vector3(cloudSize, cloudSize, 1);
+            previousResources = resources;
         }
-        resourceCloud.transform.localScale = new Vector3(cloudSize, cloudSize, 1);
 	}
 }
