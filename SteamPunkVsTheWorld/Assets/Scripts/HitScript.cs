ca2f936789ce3de33420b4c997a9131c0eb57e6a
Vamiyn <vamiynr@gmail.com>
2015-10-19 19:24:20 +0300

    hit effect basic

--- SteamPunkVsTheWorld/Assets/Scripts/HitScript.cs
+++ SteamPunkVsTheWorld/Assets/Scripts/HitScript.cs
@@ -16,7 +16,9 @@ public class HitScript : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.gameObject.tag.Equals ("Enemy")) {
 			other.SendMessageUpwards ("Damaged");
-			Instantiate(Resources.Load("HitEffect"));
+			GameObject hit = (GameObject)Instantiate(Resources.Load("HitEffect"));
+			hit.transform.position = gameObject.transform.position;
+			hit.transform.Translate (new Vector3(0, 0.37f, 0));
 			Destroy(gameObject);
 		}
 	}
