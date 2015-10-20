14f875df3775096991abbcbf130fcf6b06f21a6f
Vamiyn <vamiynr@gmail.com>
2015-10-20 19:26:40 +0300

    some menu sounds are in now

--- SteamPunkVsTheWorld/Assets/Scripts/MusicControls.cs
+++ SteamPunkVsTheWorld/Assets/Scripts/MusicControls.cs
@@ -2,22 +2,31 @@
 using System.Collections;
 
 public class MusicControls : MonoBehaviour {
-	
-    public AudioClip music;
-    private AudioSource source;
-	
+
+	private AudioSource[] audios;
+	private AudioSource music;
+
     void Awake() {
 		DontDestroyOnLoad(transform.gameObject);
-        source = GetComponent<AudioSource>();
+		audios = gameObject.GetComponents<AudioSource>();
+        music = audios[0];
     }
 
     void OnLevelWasLoaded(int level) {
         if ((level == 0)) {
-            source.Stop();
+            music.Stop();
         }
-        if (level == 1 && !source.isPlaying) {
-            source.clip = music;
-            source.Play();
+        if (level == 1 && !music.isPlaying) {
+            music.Play();
         }
     }
+
+	public void playMenuSound(bool audio) {
+		// just to make it not go over the max index
+		if (audio) {
+			audios [1].PlayOneShot (audios [1].clip);
+		} else {
+			audios [2].PlayOneShot (audios [2].clip);
+		}
+	}
 }
