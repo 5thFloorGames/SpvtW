5ed37c2311dc91ff79932a310c8024dc74068dba
Vamiyn <vamiynr@gmail.com>
2015-10-20 14:15:09 +0300

    eater animations in place

--- SteamPunkVsTheWorld/Assets/Scripts/EaterEating.cs
+++ SteamPunkVsTheWorld/Assets/Scripts/EaterEating.cs
@@ -4,18 +4,19 @@ using System.Collections;
 public class EaterEating : MonoBehaviour {
 
 	public bool digesting = false;
+	private Animator animator;
 
 	void OnTriggerEnter2D(Collider2D other) {
-		if (other.gameObject.tag.Equals("Enemy")){
+		if (other.gameObject.tag.Equals("Enemy") && !digesting){
 			digesting = true;
-			Destroy(other.gameObject);
+			StartCoroutine(timeToBite(other.gameObject));
 			Invoke("Digested", 40);
 		}
 	}
 
 	// Use this for initialization
 	void Start () {
-	
+		animator = transform.parent.gameObject.GetComponentInChildren<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -23,6 +24,15 @@ public class EaterEating : MonoBehaviour {
 	}
 
 	void Digested(){
+		animator.SetBool("Eating", false);
 		digesting = false;
 	}
+
+	IEnumerator timeToBite(GameObject other) {
+		animator.SetTrigger("Bite");
+		animator.SetBool("Eating", true);
+		yield return new WaitForSeconds(1);
+		Destroy(other);
+	}
+
 }
