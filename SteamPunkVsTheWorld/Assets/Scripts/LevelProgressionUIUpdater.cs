d8c8e61e6cf8c6df33e9d1544a4ef3cb4a3cea11
Vamiyn <vamiynr@gmail.com>
2015-10-20 17:14:43 +0300

    levelprogressionbar

---
+++ SteamPunkVsTheWorld/Assets/Scripts/LevelProgressionUIUpdater.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LevelProgressionUIUpdater : MonoBehaviour {
+	private Image levelProgress;
+	
+	void Start () {
+		levelProgress = gameObject.GetComponent<Image>();
+	}
+
+	void Update () {
+		
+	}
+
+	void updateProgressBar (float progressStatus) {
+		levelProgress.fillAmount = progressStatus;
+	}
+}
