86dca36f8231f2899238de23aa8c1864f7a458dc
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2015-10-20 12:06:43 +0300

    Eating logic

---
+++ SteamPunkVsTheWorld/Assets/Scripts/EaterEating.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EaterEating : MonoBehaviour {
+
+	public bool digesting = false;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag.Equals("Enemy")){
+			digesting = true;
+			Destroy(other.gameObject);
+			Invoke("Digested", 40);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void Digested(){
+		digesting = false;
+	}
+}
